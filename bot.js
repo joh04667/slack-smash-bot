b3aacf0f6df2e32e1f307618bb3fd12434217804
joh04667 <joh04667@gmail.com>
2016-10-19 15:55:10 -0500

    adds some utils for talking with challonge api

--- bot.js
+++ bot.js
@@ -2,9 +2,12 @@ var SmashBot = require('./lib/smashbot');
 require('dotenv').config()
 
 
+
+
 var smashbot = new SmashBot({
   token: process.env.SLACK_TOKEN,
-  name: 'smashbot'
+  name: 'smashbot',
+  challonge_key: process.env.CHALLONGE_KEY
 })
 
 smashbot.run()
