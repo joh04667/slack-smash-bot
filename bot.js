9650ad3ca4f56c8de0bc78c83859698bbd70fd8d
joh04667 <joh04667@gmail.com>
2016-11-08 23:04:15 -0600

    branch

--- bot.js
+++ bot.js
@@ -7,7 +7,8 @@ require('dotenv').config()
 var smashbot = new SmashBot({
   token: process.env.SLACK_TOKEN,
   name: 'smashbot',
-  challonge_key: process.env.CHALLONGE_KEY
+  challonge_key: process.env.CHALLONGE_KEY,
+  owner: 'kyle.johnson'
 })
 
 smashbot.run()
