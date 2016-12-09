ebc7ca5ce357b18f732435dca2b28bf98f10af3f
joh04667 <joh04667@gmail.com>
2016-12-09 09:15:55 -0600

    fixed api tokens

--- bot.js
+++ bot.js
@@ -4,10 +4,19 @@ var SmashBot = require('./lib/smashbot');
 
 
 var smashbot = new SmashBot({
-  token: process.env.SLACK_KEY,
+  token: process.env.SLACK_TOKEN,
   name: 'smashbot',
   challonge_key: process.env.CHALLONGE_KEY,
   owner: 'kyle.johnson'
 })
 
-smashbot.run()
+
+var testbot = new SmashBot({
+  token: process.env.TAP_TOKEN,
+  name: 'smashbot',
+  challonge_key: process.env.CHALLONGE_KEY,
+  owner: 'kyle.johnson'
+})
+
+smashbot.run();
+testbot.run();
