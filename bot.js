582194212972a227be91694f66955004584f707d
joh04667 <joh04667@gmail.com>
2016-12-09 08:38:07 -0600

    adds http listen so heroku doesnt throw a fit. webhook still main

--- bot.js
+++ bot.js
@@ -1,10 +1,14 @@
-// require('dotenv').config()
+require('dotenv').config();
+var http = require('http');
+
+
+http.listen(process.env.PORT || 5000)
 
 var SmashBot = require('./lib/smashbot');
 
 
 var smashbot = new SmashBot({
-  token: process.env.TAP_TOKEN,
+  token: process.env.SLACK_KEY,
   name: 'smashbot',
   challonge_key: process.env.CHALLONGE_KEY,
   owner: 'kyle.johnson'
