899a5631085d4ff4752df96f626a87ea3233c36d
joh04667 <joh04667@gmail.com>
2016-12-16 14:26:35 -0600

    debug

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ var SmashBot = require('./lib/smashbot');
 
 
 var smashbot = new SmashBot({
-  token: process.env.SLACK_TOKEN,
+  token: process.env.TAP_TOKEN,
   name: 'smashbot',
   challonge_key: process.env.CHALLONGE_KEY,
   owner: 'kyle.johnson'
